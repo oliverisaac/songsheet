15aac27c392b8483574922178384a056fe63525c
Oliver Isaac <oisaac@gmail.com>
2023-01-21 08:18:27 -0600

    auto fix apostrophes in shell script that geneates lyrics

--- generate-lyrics.sh
+++ generate-lyrics.sh
@@ -1,19 +1,24 @@
 #!/usr/bin/env bash
 
 set -eEuo pipefail
+shopt -s inherit_errexit
+
+function fix_apostrophes() {
+	sed -e "s/’/'/g" # fix "fancy" apostrophes
+}
 
 for f in songs/song.*.txt; do
-    num=$(cut -d. -f2 <<<"$f")
-    title=$(head -n 1 "$f" | sed -r -e 's/^.*?[.] *//')
-    lyrics="$(tail -n +2 "$f")"
-    jq_args=(
-        -n
-        --arg num "$num"
-        --arg title "$title"
-        --arg lyrics "$lyrics"
-        '{ number: $num | tonumber, title: $title, lyrics: $lyrics }'
-    )
-    jq "${jq_args[@]}"
+	num=$(cut -d. -f2 <<<"$f")
+	title=$(head -n 1 "${f}" | fix_apostrophes | sed -r -e 's/^.*?[.] *//')
+	lyrics="$(tail -n +2 "${f}" | fix_apostrophes)"
+	jq_args=(
+		-n
+		--arg num "${num}"
+		--arg title "${title}"
+		--arg lyrics "${lyrics}"
+		'{ number: $num | tonumber, title: $title, lyrics: $lyrics }'
+	)
+	jq "${jq_args[@]}"
 done |
-    jq --slurp '. | sort_by(.number) | { songs: . } | tojson' | 
-    sed 's/^/var songs = JSON.parse(/; s/$/)/;'
+	jq --slurp '. | sort_by(.number) | { songs: . } | tojson' |
+	sed 's/^/var songs = JSON.parse(/; s/$/)/;'
